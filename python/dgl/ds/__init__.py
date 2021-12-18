bb878181f9f477e9e8021a21f48b08328a1ac5ba
czkkkkkk <zekucai@gmail.com>
2021-12-18 07:38:45 +0000

    Add sampling interface

--- python/dgl/ds/__init__.py
+++ python/dgl/ds/__init__.py
@@ -3,8 +3,7 @@ from .._ffi.function import _init_api
 import torch as th
 
 from .context import DSContext
-
-
+from . import sampling
 
 DS_CONTEXT = None
 
@@ -17,5 +16,7 @@ def init(rank, world_size):
     th.distributed.broadcast(id, 0)
     _CAPI_DGLNCCLInit(bytearray(id.byte()), rank, world_size, DS_CONTEXT)
 
+def sample_neighbors(g, seeds, fanout):
+    return sampling.sample_neighbors(g, seeds, fanout, DS_CONTEXT)
 
 _init_api("dgl.ds")
