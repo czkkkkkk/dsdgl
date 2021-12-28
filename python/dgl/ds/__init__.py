c9776e6e687c880a49dc12c0a2cc6923e90ae803
czkkkkkk <zekucai@gmail.com>
2021-12-28 04:39:04 +0000

    Refactor

--- python/dgl/ds/__init__.py
+++ python/dgl/ds/__init__.py
@@ -2,21 +2,12 @@ from .._ffi.function import _init_api
 
 import torch as th
 
-from .context import DSContext
-from . import sampling
-
-DS_CONTEXT = None
+from .sampling import sample_neighbors
 
 def init(rank, world_size):
-    global DS_CONTEXT
-    DS_CONTEXT = DSContext.create(rank)
-
     id = _CAPI_DGLNCCLGetUniqueId(rank)
     id = th.ByteTensor(id)
     th.distributed.broadcast(id, 0)
-    _CAPI_DGLNCCLInit(bytearray(id.byte()), rank, world_size, DS_CONTEXT)
-
-def sample_neighbors(g, num_vertices, device_min_vids, device_min_eids, nodes, fanout):
-    return sampling.sample_neighbors(g, num_vertices, device_min_vids, device_min_eids, nodes, fanout, DS_CONTEXT)
+    _CAPI_DGLNCCLInit(bytearray(id.byte()), rank, world_size)
 
 _init_api("dgl.ds")
