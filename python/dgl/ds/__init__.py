0023993bfe7cb3f9fc7cc7c7dd658ed19f00a01a
czkkkkkk <zekucai@gmail.com>
2022-01-01 09:34:21 +0000

    Add coordinator

--- python/dgl/ds/__init__.py
+++ python/dgl/ds/__init__.py
@@ -6,9 +6,6 @@ from .sampling import sample_neighbors
 from .to_block import to_block
 
 def init(rank, world_size):
-    id = _CAPI_DGLNCCLGetUniqueId(rank)
-    id = th.ByteTensor(id)
-    th.distributed.broadcast(id, 0)
-    _CAPI_DGLNCCLInit(bytearray(id.byte()), rank, world_size)
+    _CAPI_DGLDSInitialize(rank, world_size)
 
 _init_api("dgl.ds")
