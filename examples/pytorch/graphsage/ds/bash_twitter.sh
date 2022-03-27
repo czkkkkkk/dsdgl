769d13a3abeff12ed1a63361988fb01a17092921
joeyzhouqihui <joeyzhouqihui@gmail.com>
2022-03-27 05:14:29 +0000

    add dataset to partition

--- examples/pytorch/graphsage/ds/bash_twitter.sh
+++ examples/pytorch/graphsage/ds/bash_twitter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python partition_graph.py --dataset=twitter --num_parts=2 --output=/data/ds/twitter_2
+python partition_graph.py --dataset=twitter --num_parts=2 --output=/data/ds/twitter_2 --undirected=False
 sleep 20
-python partition_graph.py --dataset=twitter --num_parts=4 --output=/data/ds/twitter_4
+python partition_graph.py --dataset=twitter --num_parts=4 --output=/data/ds/twitter_4 --undirected=False
 sleep 20
-python partition_graph.py --dataset=twitter --num_parts=8 --output=/data/ds/twitter_8
+python partition_graph.py --dataset=twitter --num_parts=8 --output=/data/ds/twitter_8 --undirected=False
 sleep 20
-python partition_graph.py --dataset=twitter --num_parts=1 --output=/data/ds/twitter_1
+python partition_graph.py --dataset=twitter --num_parts=1 --output=/data/ds/twitter_1 --undirected=False
--- examples/pytorch/graphsage/ds/bash_web.sh
+++ examples/pytorch/graphsage/ds/bash_web.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python partition_graph.py --dataset=webuk --num_parts=1 --output=/data/ds/web_uk_1
+python partition_graph.py --dataset=webuk --num_parts=1 --output=/data/ds/web_uk_1 --undirected=False
 sleep 20
-python partition_graph.py --dataset=webuk --num_parts=2 --output=/data/ds/web_uk_2
+python partition_graph.py --dataset=webuk --num_parts=2 --output=/data/ds/web_uk_2 --undirected=False
 sleep 20
-python partition_graph.py --dataset=webuk --num_parts=4 --output=/data/ds/web_uk_4
+python partition_graph.py --dataset=webuk --num_parts=4 --output=/data/ds/web_uk_4 --undirected=False
 sleep 20
-python partition_graph.py --dataset=webuk --num_parts=8 --output=/data/ds/web_uk_8
+python partition_graph.py --dataset=webuk --num_parts=8 --output=/data/ds/web_uk_8 --undirected=False
