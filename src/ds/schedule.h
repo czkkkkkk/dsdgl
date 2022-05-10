8fb809dd217de302e5029856e19c9ba063259f89
czkkkkkk <zekucai@gmail.com>
2022-05-10 12:43:48 +0000

    Fix zmq problem when exit the program

--- src/ds/schedule.h
+++ src/ds/schedule.h
@@ -19,19 +19,32 @@ using namespace dgl::aten;
 namespace ds {
 
 class Scheduler {
+  static constexpr int STOP_SIGNAL = -2;
 public:
   // use stream_t value as token
-  Scheduler() : comm_tokens_(50), comm_requests_(50), cur_token_(-1) {}
+  Scheduler() : comm_tokens_(50), comm_requests_(50), cur_token_(-1), alive_(true) {}
+  ~Scheduler() {
+    auto* ds_context = DSContext::Global();
+    int rank = ds_context->rank;
+    if(rank == 0) {
+      comm_requests_.Put(STOP_SIGNAL);
+    }
+    while(alive_);
+  }
 
   void Schedule() {
     auto* ds_context = DSContext::Global();
     int next_token, expect = -1;
     while (true) {
       next_token = comm_tokens_.Get();
+      if (next_token == STOP_SIGNAL) {
+        break;
+      }
       while (!cur_token_.compare_exchange_weak(expect, next_token)) {
         expect = -1;
       }
     }
+    alive_ = false;
   }
 
   void Coordinate() {
@@ -45,6 +58,9 @@ public:
       ds_context->comm_coordinator->Barrier();
       ds_context->comm_coordinator->Broadcast(next_token);
       comm_tokens_.Put(next_token);
+      if(next_token == STOP_SIGNAL) {
+        break;
+      }
     }
   }
 
@@ -76,7 +92,7 @@ public:
 private:
   Buffer<int> comm_tokens_;
   Buffer<int> comm_requests_;
-  std::atomic<int> cur_token_;
+  std::atomic<int> cur_token_, alive_;
 
 };
 
